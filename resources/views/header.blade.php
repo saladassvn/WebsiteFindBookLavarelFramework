c2d47d8a8d731ebaa0d1854a23305dd95abeeb9a
saladassvn <buiquocdat321@gmail.com>
2022-07-12 17:10:55 +0700

    add chuc nang quay lai trang Admin

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -21,14 +21,16 @@
                             Cửa hàng bán sách hàng đầu Findbook
                             <!-- Begin:dau QR Code -->
                             <div class="dau__qr">
-                                <img src="{{ '../public/User/img/qr_code.png' }}" alt="QR code" class="dau__qr-img">
+                            
+                                <img src="{{asset('public/User/img/qr_code.png')}}" alt="QR code" class="dau__qr-img">
                                 <div class="dau__qr-apps">
-                                    <a href="" class="dau__qr-link">
-                                        <img src="{{ '../public/User/img/google_play.png' }}" alt="Google Play"
+                                    <a href="" class="dau__qr-link">                                  
+                                        <img src="{{asset('public/User/img/google_play.png')}}" alt="Google Play"
                                             class="dau__qr-download-img">
                                     </a>
+                                    
                                     <a href="" class="dau__qr-link">
-                                        <img src="{{ '../public/User/img/app_store.png' }}" alt="App Store"
+                                        <img src="{{asset('public/User/img/app_store.png')}}" alt="App Store"
                                             class="dau__qr-download-img">
                                     </a>
                                 </div>
@@ -60,7 +62,8 @@
                                 <ul class="dau__notify-list">
                                     <li class="dau__notify-item dau__notify-item--viewed">
                                         <a href="" class="dau__notify-link">
-                                            <img src="{{ 'public/frontend/img/CleanCode.png' }}" alt=""
+                                        
+                                            <img src="{{asset('public/User/img/CleanCode.png')}}" alt=""
                                                 class="dau__notify-img">
                                             <div class="dau__notify-info">
                                                 <span class="dau__notify-name">Bạn đã đặt Clean Code</span>
@@ -73,7 +76,8 @@
                                 <ul class="dau__notify-list">
                                     <li class="dau__notify-item">
                                         <a href="" class="dau__notify-link">
-                                            <img src="{{ 'public/frontend/img/the_clean_coder.jpg' }}" alt=""
+                                        
+                                            <img src="{{asset('public/User/img/the_clean_coder.jpg')}}" alt=""
                                                 class="dau__notify-img">
                                             <div class="dau__notify-info">
                                                 <span class="dau__notify-name">Bạn đã đặt The Clean Code</span>
@@ -86,7 +90,8 @@
                                 <ul class="dau__notify-list">
                                     <li class="dau__notify-item">
                                         <a href="" class="dau__notify-link">
-                                            <img src="{{ 'public/frontend/img/CodeComplete2.jpg' }}" alt=""
+                                        
+                                            <img src="{{asset('public/User/img/CodeComplete2.jpg')}}" alt=""
                                                 class="dau__notify-img">
                                             <div class="dau__notify-info">
                                                 <span class="dau__notify-name">Bạn đã đặt Code Complete 2</span>
@@ -106,12 +111,6 @@
                                 <i class="dau__navbar-icon far fa-question-circle"></i>
                                 Trợ giúp
                             </a>
-                        <li class="dau__navbar-item">
-                            <a href="" class="dau__navbar-item-link">
-                                <i class="dau__navbar-icon far fa-question-circle"></i>
-                                Trợ giúp
-                            </a>
-                        </li>
                         @if (Session::has('userName'))
                             <li class="dau__navbar-user-item">
                                 <a href="{{ URL::to('/user') }}">{{ Session::get('userName') }}</a>
@@ -123,9 +122,13 @@
                                 <a href="{{ URL::to('/logout') }}">Đăng xuất</a>
                             </li>
                         @elseif(Session::has('AdminName'))
-                             <li class="dau__navbar-user-item dau__navbar-user-item--separate">
+                            <li class="dau__navbar-user-item">
+                                <a href="{{ URL::to('admin/IndexAdmin') }}">Trang Admin</a>
+                            </li>
+                             <li class="dau__navbar-user-item">
                                 <a href="{{ URL::to('admin/logout') }}">Đăng xuất</a>
                             </li>
+ 
                         @else
                             <li class="dau__navbar-item dau__navbar-user">
                             <li class="dau__navbar-user-item dau__navbar-user-item--separate  ;">
